4ca0b5a853a8815eb1fdec3a50de1e190b1bf146
Elias Dorneles <eliasdorneles@gmail.com>
2013-05-04 15:54:31 -0300

    moved page to index.html and adjusted app.yaml and main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
 
-print 'Context-type: text/plain'
-print
-# just print this file, for now
-print open('fretboard/jsfretboard.html').read()
+import webapp2
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.error(404)
+        self.response.out.write('<h1>404 Not Found</h1>Sorry, nothing here yet')
+app = webapp2.WSGIApplication([
+    ('/hello', MainHandler),
+    ], debug=True)
